8871c3c94a5fb72486ae7e5676930e04da0065c5
Nazira-umucyo <n.umucyo@alustudent.com>
2025-09-14 16:17:01 +0200

    min

--- minimum_operations/0-minoperations.py
+++ minimum_operations/0-minoperations.py
@@ -1,30 +1,17 @@
 #!/usr/bin/python3
-"""
-Module to calculate minimum operations to get n H characters.
-"""
+""" Minimum Operations"""
+
 
 def minOperations(n):
-        """
-            Calculate the minimum number of operations (Copy All and Paste)
-                to get exactly n 'H' characters in a file.
-                    
-                        Args:
-                                n (int): Target number of H characters.
-                                        
-                                            Returns:
-                                                    int: Minimum number of operations, or 0 if impossible.
-                                                        """
-                                                            if n < 2:
-                                                                        return 0  # Can't do anything for n < 2
-                                                                        
-                                                                        operations = 0
-                                                                            divisor = 2
-
-                                                                                while n > 1:
-                                                                                            while n % divisor == 0:
-                                                                                                            operations += divisor
-                                                                                                                        n //= divisor
-                                                                                                                                divisor += 1
-
-                                                                                                                                    return operations
-
+        """ Minimum Operations"""
+            if n <= 1:
+                        return 0
+                        i = 2
+                            result = 0
+                                while i <= n:
+                                            if n % i == 0:
+                                                            result += i
+                                                                        n = n / i
+                                                                                else:
+                                                                                                i += 1
+                                                                                                    return result
